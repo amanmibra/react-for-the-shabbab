11815318845ba19847def275f8b9c719fc8474e6
Aman Ibrahim <amanmibra@gmail.com>
2020-01-26 16:48:27 -0500

    Added multiplier

--- src/Cookie.jsx
+++ src/Cookie.jsx
@@ -3,12 +3,18 @@ import React, { Component } from 'react';
 class Cookie extends Component {
 
   render() {
-    const numOfCookies = this.props.numOfCookies; //TODO
+    const { multiplier, numOfCookies } = this.props; //TODO
     const cookies = []
     for (let i = 0; i < numOfCookies; i++) {
-      cookies.push(
-        <img src  ="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/apple/81/cookie_1f36a.png"/>
-      )
+      if (multiplier == 10) {
+        cookies.push(
+          '⭐'
+        )
+      } else {
+        cookies.push(
+          <img key={i} src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/apple/81/cookie_1f36a.png"/>
+        )
+      }
     }
     return (
       <div>
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -8,31 +8,60 @@ class Counter extends Component {
     super(props);
     this.state = {
       count: 0,
-      test: '',
+      multiplier: 0,
+      previousClick: '',
     }
   }
 
   addCount = () => {
+    if (this.state.previousClick == 'add') {
+      this.setState({
+        multiplier: this.state.multiplier + 1,
+      })
+
+    } else {
+      this.setState({
+        multiplier: 0,
+      })
+    }
+
     this.setState({
       count: this.state.count + 1,
+      previousClick: 'add',
     })
   }
 
   minusCount = () => {
+    if (this.state.previousClick == 'minus') {
+      this.setState({
+        multiplier: this.state.multiplier + 1,
+      })
+
+    } else {
+      this.setState({
+        multiplier: 0,
+      })
+    }
     this.setState({
       count: this.state.count - 1,
+      previousClick: 'minus',
     })
   }
 
   render() {
+    const { multiplier } = this.state;
     return (
       <div>
+        {
+          multiplier != 0 ? `x${multiplier}` : null
+        }
+        <br />
         {this.state.count} steps
         <div>
           <button type="button" onClick={this.addCount}>+ Add</button>
           <button type="button" onClick={this.minusCount}>- Minus</button>
 
-          <Cookie numOfCookies={this.state.count / 5}/>
+          <Cookie multiplier={multiplier} numOfCookies={this.state.count / 5}/>
         </div>
       </div>
     );
